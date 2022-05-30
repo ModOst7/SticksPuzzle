0f4333bdfa6066e4ee13477f9d8b53d83015f103
ModOst7 <karasique359@yandex.ru>
2022-05-30 22:22:01 +0400

    3

--- src/Teacher/Teacher.jsx
+++ src/Teacher/Teacher.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring'
+import './Teacher.css'
 
 
 const Teacher = () => {
   //const AnimatedImage = animated(Image);
   const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, delay: 1000 })
     return (
-    <animated.img style={props} src='./zh.png' width={window.innerWidth/8} />
+      <div className='teacherContainer'>
+        <animated.img className='teacherImg' style={props} src='./zh.png'  />
+        <div className='message'></div>
+      </div>
     )
   };
 
